f27f9dbf57b619875e75c1b23ec0e0271dcab3f0
Asef264 <xhmoazedi@gmail.com>
2024-07-06 10:28:00 +0330

    init database

---
+++ pkg/infrastructure/database/pg/pgdb.go
@@ -0,0 +1,25 @@
+package pg
+
+import (
+	"fmt"
+
+	_ "github.com/lib/pq"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var Pgdb *gorm.DB
+
+func PgInit() {
+	defer func() {
+		msg := recover()
+		fmt.Println(msg)
+	}()
+	dsn := "host=localhost user=user password=password dbname=moaForm sslmode=disable"
+	pdb, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("db connected ...")
+	Pgdb = pdb
+}
